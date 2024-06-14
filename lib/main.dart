ce8034f157a65472baaca4204a7ed2e10e86a735
etec@devisate.com.br <etec@devisate.com.br>
2024-06-14 08:30:26 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/src/app.dart';
+
+void main() {
+  runApp(const App());
+}
+
---
+++ lib/src/model/music_model.dart
@@ -0,0 +1,8 @@
+class MusicModel {
+  final int    musicID;
+  final String name;
+  final String authorName;
+  final dynamic music;
+
+  MusicModel({required this.musicID, required this.name, required this.authorName, required this.music});
+}
